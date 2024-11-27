4672a4fb9afe448584def41c322e57a13a9960d5
SITT YAN HTUN <sittyanhtun@SITTs-MacBook-Air.local>
2024-11-27 19:35:32 +0700

    Uploading Project that did during HND Course

---
+++ build/bluetooth_classic/generated/source/buildConfig/release/com/matteogassend/bluetooth_classic/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.matteogassend.bluetooth_classic;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.matteogassend.bluetooth_classic";
+  public static final String BUILD_TYPE = "release";
+}
